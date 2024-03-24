54159ce312ee7eecb06acfd7acbd91d4f9bcf2f7
n3jwm3 <sabrhai46@gmail.com>
2024-03-24 15:40:54 +0100

    jsjj

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,5 +1,18 @@
 @extends('base')
 @section('title','about')
 @section('content')
-<h1>about</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/hom.css')}}">
+</head>
+<body class="do">
+    <h1>about</h1>
+</body>
+</html>
+
 @endsection
